ef24c42cba0df09ee62fb450dcc11d62e7d7fd81
Indusha <im2semba@uwaterloo.ca>
2022-03-23 02:59:45 -0400

    changes to cal

--- capstonefrontend/src/Components/AppointmentComponents/CreateAppointmentV1Component.js
+++ capstonefrontend/src/Components/AppointmentComponents/CreateAppointmentV1Component.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import { Container, Row, Col } from 'react-bootstrap';
+import { Modal, Button, ListGroup } from "react-bootstrap";
 
 function CreateAppointmentV1Component() {
   const [value, onChange] = useState(new Date());
-  
+  const [showModal, setShow] = useState(false);
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
 
  
 
@@ -21,7 +25,7 @@ function CreateAppointmentV1Component() {
             md={{ span: 6, order: 1 }}
             lg={{ span: 8, order: 1 }}>
             
-            <Calendar onChange={onChange} value={value}/>
+            <Calendar style={{ width: '200%' }} onChange={onChange} value={value}/>
           
           </Col>
           <Col
@@ -30,21 +34,42 @@ function CreateAppointmentV1Component() {
             md={{ span: 6, order: 2 }}
             lg={{ span: 3, order: 1 }}>
             <div style={{ maxWidth: 250 }}>
-            <ul class="list-group">
-            {(value.toDateString())}
-            <li class="list-group-item">8AM - 9PM Price: $34</li>
-            <li class="list-group-item">11AM - 12PM  Price: $24</li>
-            <li class="list-group-item">1PM - 2PM  Price: $44 </li>
-            <li class="list-group-item">3PM - 5PM Price: $31 </li>
-            <li class="list-group-item">6PM - 7PM Price: $25 </li>
+            <ListGroup class="list-group">
+            <h4>{(value.toDateString())}</h4>
+            <ListGroup.Item action variant="primary" onClick={handleShow} class="list-group-item">8AM - 9AM Price: $34</ListGroup.Item>
+            <ListGroup.Item action variant="primary" class="list-group-item">11AM - 12PM  Price: $24</ListGroup.Item>
+            <ListGroup.Item action variant="primary" class="list-group-item">1PM - 2PM  Price: $44 </ListGroup.Item>
+            <ListGroup.Item action variant="primary" class="list-group-item">3PM - 5PM Price: $31 </ListGroup.Item>
+            <ListGroup.Item action variant="primary" class="list-group-item">6PM - 7PM Price: $25 </ListGroup.Item>
+
+
             
-        </ul>
+        </ListGroup>
+
+       
         </div>
           </Col>
         </Row>
       </Container>
     </div>
+    
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Booking</Modal.Title>
+        </Modal.Header>
+        <Modal.Body> Would you like to book: {(value.toDateString())} 8AM - 9PM </Modal.Body>
+        <Modal.Body>  Price: $34</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
   </div>
+  
       
       
 
